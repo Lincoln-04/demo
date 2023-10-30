2eb203921f76144749ada1e71c81b4fbd2b6b520
Lincoln-04 <146818672+Lincoln-04@users.noreply.github.com>
2023-10-30 12:07:48 +0530

    third commit

--- demo/src/pack1/Welcome.java
+++ demo/src/pack1/Welcome.java
@@ -4,7 +4,8 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to this project");.
+		System.out.println("Welcome to this project");
+		System.out.println("Welcome to CSE");
 	}
 
 }
